7e884fb1d7a8f624496d29272db567fe42f2cdc9
B <hennyhexservers@gmail.com>
2020-12-25 12:53:18 +0000

    cleanup

--- src/main/java/io/github/hhservers/bungeebroadcast/BungeeBroadcast.java
+++ src/main/java/io/github/hhservers/bungeebroadcast/BungeeBroadcast.java
@@ -1,8 +1,8 @@
 package io.github.hhservers.bungeebroadcast;
 
+import lombok.SneakyThrows;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.api.scheduler.ScheduledTask;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
@@ -13,33 +13,37 @@ import java.util.concurrent.TimeUnit;
 
 public final class BungeeBroadcast extends Plugin {
 
-    private static BungeeBroadcast plugin;
+    public static BungeeBroadcast plugin;
     private File file;
     private Configuration configuration;
 
 
+    @SneakyThrows
     @Override
     public void onEnable() {
         plugin = this;
         getLogger().info("has loaded!");
-        if(!getDataFolder().exists()){
+
+        if (!getDataFolder().exists()) {
             getDataFolder().mkdir();
         }
         file = new File(getDataFolder(), "config.yml");
         try {
-            if(!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
-            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
-            ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, file);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for(String key : configuration.getSection("broadcast").getKeys()){
-            String message = configuration.getString("broadcast."+key+".message");
-            long interval = configuration.getInt("broadcast."+key+".interval");
-            if(configuration.getString("broadcast."+key+".url").isEmpty()) {
+        configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+        ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, file);
+
+
+        for (String key : configuration.getSection("broadcast").getKeys()) {
+            String message = configuration.getString("broadcast." + key + ".message");
+            long interval = configuration.getInt("broadcast." + key + ".interval");
+            if (configuration.getString("broadcast." + key + ".url").isEmpty()) {
                 ProxyServer.getInstance().getScheduler().schedule(this, new Runnable() {
                     public void run() {
                         new Util().sendBroadcast(message);
@@ -47,7 +51,7 @@ public final class BungeeBroadcast extends Plugin {
                     }
                 }, 1, interval, TimeUnit.MINUTES);
             } else {
-                String url = configuration.getString("broadcast."+key+".url");
+                String url = configuration.getString("broadcast." + key + ".url");
                 ProxyServer.getInstance().getScheduler().schedule(this, new Runnable() {
                     public void run() {
                         new Util().sendURLBroadcast(message, url);
@@ -61,5 +65,9 @@ public final class BungeeBroadcast extends Plugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
     }
 }
